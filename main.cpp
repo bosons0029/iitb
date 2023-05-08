d29a32afd6d05d1901b98de1d092242dde15e240
Subhendra Basu <basu.subhendra@outlook.com>
2023-05-08 12:41:26 +0530

    Added All Video controls , in principle, size, position to be fixed next, followed by connection to video backend, which will complete the project , after testing

--- main.cpp
+++ main.cpp
@@ -1,65 +1,26 @@
 #include <wx/wx.h>
+#include <wx/slider.h>
+#include <wx/choice.h>
+#include <wx/button.h>
+#include <wx/animate.h>
 
-enum {
-    ID_Hello = 1
-};
-
-class MyApp: public wxApp {
+class VideoCaptureApp: public wxApp {
     public:
-        bool OnInit() override;
+        bool OnInit();
 };
 
-wxIMPLEMENT_APP(MyApp);
+wxIMPLEMENT_APP(VideoCaptureApp);
 
-class MyFrame: public wxFrame {
-    public:
-        MyFrame();
-   
-    private:
-        void OnHello(wxCommandEvent& event);
-        void OnExit(wxCommandEvent& event);
-        void OnAbout(wxCommandEvent& event);
-};
+bool VideoCaptureApp::OnInit() {
+    wxFrame* frame = new wxFrame(nullptr, wxID_ANY, argv[0]);
+    frame->Show(true);
 
-MyFrame::MyFrame():
-    wxFrame(nullptr, wxID_ANY, "Hello World") {
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl+H", "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
-
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
-
-    wxMenuBar* menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
-
-    SetMenuBar(menuBar);
-
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
-
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
-}
-
-void MyFrame::OnExit(wxCommandEvent& event) {
-    Close(true);
-}
-
-void MyFrame::OnAbout(wxCommandEvent& event) {
-    wxMessageBox("This is a wxWidgets Hello World example", "About Hello World", wxOK | wxICON_INFORMATION);
-}
-
-void MyFrame::OnHello(wxCommandEvent& event) {
-    wxLogMessage("Hello world from wxWidgets!");
-}
-
-bool MyApp::OnInit() {
-    MyFrame* frame = new MyFrame();
-    frame->Show();
+    wxChoice* video_input_source = new wxChoice(frame, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0, NULL, 0, wxDefaultValidator, "Select Video Source");
+    wxButton* start_button = new wxButton(frame, wxID_ANY, "Start", wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, "Start");
+    wxButton* stop_button = new wxButton(frame, wxID_ANY, "Stop", wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, "Stop");
+    wxSlider* brightness_slider = new wxSlider(frame, wxID_ANY, 0, 0, 50, wxDefaultPosition, wxDefaultSize, wxSL_HORIZONTAL, wxDefaultValidator, "Brightness");
+    wxSlider* contrast_slider = new wxSlider(frame, wxID_ANY, 0, 0, 50, wxDefaultPosition, wxDefaultSize, wxSL_HORIZONTAL, wxDefaultValidator, "Contrast");
+    wxAnimationCtrl* video_stream = new wxAnimationCtrl(frame, wxID_ANY, wxNullAnimation, wxDefaultPosition, wxDefaultSize, wxAC_DEFAULT_STYLE, "Video Stream");
     return true;
 }
 
