c9f0ab03b152eb912f2271b23906a748ecc286fd
Subhendra Basu <basu.subhendra@outlook.com>
2023-05-09 06:21:48 +0530

    Partial Solution Part 2.

--- part2.sln/main2.cpp
+++ part2.sln/main2.cpp
@@ -3,20 +3,30 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
+#include <dirent.h>
 using namespace std;
 using namespace cv;
 
-
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        cout << "Usage: ./part2 <dir of Images to be analyzed>" << endl;
-        return 2;
-    }
-    char* dir = strdup(argv[1]);
-    Mat img = imread("Image-1.jpg", 0);
-    Mat img_blur;
-    GaussianBlur(img, img_blur, Size(3,3), 0);
-    Mat edges;
-    Canny(img_blur, edges, 100, 200, 3, false);
-    return 0;
+	if (argc < 2) {
+		cout << "Usage: ./part2 <dir of Images to be analyzed>" << endl;
+		return 2;
+	}
+        struct dirent* entry;
+	char* dir = strdup(argv[1]);
+        DIR* dp = opendir(dir);
+	while ((entry = readdir(dp))) {
+		std::cout << entry->d_name << std::endl;
+                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+continue;
+}
+                std::string img_path = dir;
+                img_path = img_path + "/" + entry->d_name;
+		Mat img = imread(img_path, 0);
+		Mat img_blur;
+		GaussianBlur(img, img_blur, Size(3,3), 0);
+		Mat edges;
+		Canny(img_blur, edges, 100, 200, 3, false);
+	}
+	return 0;
 }  
